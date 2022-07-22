007e9a6dede53b1fcf37e7a9c0e91e262a7b2ebb
Joe Nathan Abellard <contact@jabellard.com>
2022-07-22 13:05:00 -0400

    Resturn IQueryable<T>

--- MartenChocolate/MartenChocolate/Query.cs
+++ MartenChocolate/MartenChocolate/Query.cs
@@ -5,9 +5,9 @@ namespace MartenChocolate;
 public class Query
 {
     [UseFiltering]
-    public IEnumerable<Book> GetBooks([Service(ServiceKind.Default)] IQuerySession querySession)
+    public IQueryable<Book> GetBooks([Service(ServiceKind.Default)] IQuerySession querySession)
     {
         var queryable = querySession.Query<Book>();
-        return queryable.ToList();
+        return queryable;
     }
 }
